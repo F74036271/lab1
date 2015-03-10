f28244771a28ee399f65f9a0af3bb22216198ed0
student <F74036271@2015cpp.(none)>
2015-03-10 20:16:58 +0800

    lab1

--- lab1.cpp
+++ lab1.cpp
@@ -5,13 +5,14 @@ int main()
 {
 	int n;
 	cin >> n;
-	cout << n << endl;
-	if (n==1)
-		;
-	else if(n%2)
-		n=3*n+2;
-	else
-		n=n/2;
-	cout << n << endl;
+	cout << n <<endl;
+	while(n!=1)
+	{
+		if(n%2)
+			n=3*n+1;
+		else
+			n=n/2;
+		cout<< n <<endl;
+	}
 	return 0;
 }
